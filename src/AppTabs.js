48117e546aaba932c83bb1211032a97ce7b2ca5b
Sieg <siegblink@gmail.com>
2020-03-16 08:57:29 +0800

    Comment out unused Home component.

--- src/AppTabs.js
+++ src/AppTabs.js
@@ -9,21 +9,21 @@ import styles from './common/CommonStyles'
 
 const Tabs = createBottomTabNavigator()
 
-function Home() {
-  const { logout } = React.useContext(AuthContext)
-  return (
-    <Center>
-      <View style={styles.home}>
-        <Text style={styles.text}>Home</Text>
-        <TouchableOpacity onPress={() => logout()}>
-          <View style={styles.button}>
-            <Text>Log out</Text>
-          </View>
-        </TouchableOpacity>
-      </View>
-    </Center>
-  )
-}
+// function Home() {
+//   const { logout } = React.useContext(AuthContext)
+//   return (
+//     <Center>
+//       <View style={styles.home}>
+//         <Text style={styles.text}>Home</Text>
+//         <TouchableOpacity onPress={() => logout()}>
+//           <View style={styles.button}>
+//             <Text>Log out</Text>
+//           </View>
+//         </TouchableOpacity>
+//       </View>
+//     </Center>
+//   )
+// }
 
 function Search() {
   const { logout } = React.useContext(AuthContext)
@@ -45,7 +45,7 @@ export default function AppTabs() {
   return (
     <Tabs.Navigator
       screenOptions={({ route }) => ({
-        tabBarIcon: ({ focused, color, size }) => {
+        tabBarIcon: ({ color, size }) => {
           let iconName
           // Checking the route name
           if (route.name === 'Home') {
